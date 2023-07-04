28af388f4b01a22a76177d99a58e0c574fd155b2
arimelf <eedeverdeen@gmail.com>
2023-07-04 16:21:37 -0700

    updated data uploading and file content reading

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask,render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from wtforms.validators import InputRequired
+import pandas as pd
+from utils import model_predict
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -27,7 +29,9 @@ def acetylcho():
         file = request.files['file']
         filename = file.filename
         if file and allowed_file(filename):
-            return render_template('acetylcho.html',success_message = 'File successfully uploaded!')
+            upload = Upload(filename=filename,data = file.read())
+            df = model_predict(upload.data.decode("utf-8"))
+            return render_template('acetylcho.html',success_message = 'File successfully uploaded!',table_data = df)
         else:
             error_message = 'Please upload a correct file(.txt)!'
             return render_template('acetylcho.html', error_message=error_message)
---
+++ utils.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import subprocess
+from chembl_webresource_client.new_client import new_client
+import pickle
+
+def model_predict(compounds_str):
+    compounds_list = list(compounds_str.split(' '))
+    print(f"compounds_list is {compounds_list}")
+    molecule = new_client.molecule
+    my_res = []
+    mols = molecule.filter(molecule_chembl_id__in=compounds_list).only(['molecule_chembl_id', 'molecule_structures'])
+    for molecules in mols:
+        my_res.append([molecules['molecule_structures']['canonical_smiles'],molecules['molecule_chembl_id']])
+
+
+    df1 = pd.DataFrame(my_res,columns = ['Canonical Smiles','Molecule ChemBL ID'])
+    df1.to_csv('molecule.smi', sep='\t', index=False, header=False)
+    subprocess.run('models/acetylcholinesterase/padel.sh', shell=True, check = True) 
+    df = pd.read_csv('models/acetylcholinesterase/data/descriptors_output.csv')
+    
+    df = df.drop(columns=['Name'])
+    
+    model = pickle.load(open("models/acetylcholinesterase/data/trained_model.pkl","rb"))
+    features = pickle.load(open("models/acetylcholinesterase/data/selected_features.pkl","rb"))
+    df = df[features]
+    print(f"datafram is {df}")
+    y_predicted = model.predict(df)
+
+    b = [pow(10,-value)*1000000000 for value in y_predicted]
+    df1['Predicted IC50 value (nM)'] = b
+
+    return df1
+
+if __name__ == '__main__':
+    my_str = "CHEMBL133897 CHEMBL336398 CHEMBL336398"
+    my_list = list(my_str.split(' '))
+    res = model_predict(my_list)
+    print(res)
