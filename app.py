cc3e05c6195891b38c1223371c2e4c3cea37be9f
arimelf <eedeverdeen@gmail.com>
2023-07-14 21:41:22 -0700

    updated url redirecting and result displaying

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask,render_template,request,send_file
+from flask import Flask,render_template,request,send_file,redirect,url_for,session
 from flask_sqlalchemy import SQLAlchemy
 from wtforms.validators import InputRequired
 import pandas as pd
 from utils import model_predict
 
 app = Flask(__name__)
+app.secret_key = 'your-secret-key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 app.app_context().push()
@@ -42,8 +43,23 @@ def upload_file():
     db.session.add(upload)
     db.session.commit()
     df.to_csv('models/acetylcholinesterase/data/user_data_'+str(upload.id)+'.csv',index=False)
-    print('success')
-    return render_template('results.html',headings = list(df),data = df.values.tolist())
+    session['headings'] = list(df)
+    session['data'] = df.values.tolist()
+    return 'File successfully uploaded'
+
+
+@app.route('/results/')
+def results():
+    headings = session.get('headings', [])
+    data = session.get('data', [])
+    
+    # Clear the session variables
+    session.pop('headings', None)
+    session.pop('data', None)
+    
+    # Pass headings and data to the results template
+    return render_template('results.html', headings=headings, data=data)
+
 
 if __name__=='__main__':
     app.run(debug=True)
