3538ff7c666237eac42fe670d9993b49fe33654a
arimelf <eedeverdeen@gmail.com>
2023-07-14 20:54:58 -0700

    updated upload file handling

--- app.py
+++ app.py
@@ -24,19 +24,7 @@ def allowed_file(filename):
     return True if filename[-3:]=='txt' else False
 
 @app.route('/acetylcholinesterase/',methods = ['GET','POST'])
-def acetylcho():
-    if request.method == 'POST':
-        file = request.files['file']
-        filename = file.filename
-        if file and allowed_file(filename):
-            filecontent = file.read().decode("utf-8")
-            df = model_predict(filecontent).sort_values('Predicted IC50 value (nM)')
-            upload = Upload(filename=filename,data = filecontent)
-            db.session.add(upload)
-            db.session.commit()
-            df.to_csv('models/acetylcholinesterase/data/user_data_'+str(upload.id)+'.csv',index=False)
-            return render_template('acetylcho.html',headings = list(df),data = df.values.tolist(),file_download = 'Download .csv file Here',id = upload.id)
-        
+def acetylcho():    
     return render_template('acetylcho.html')
 
 @app.route('/download/<variable>/')
@@ -46,16 +34,16 @@ def download_file(variable):
 
 @app.route('/upload/',methods = ['POST'])
 def upload_file():
-    # file = request.form['data']
-    # #file_content = file.read().decode('utf-8')
-    # print(type(file))
-    # print(file)
     data = request.json
-    file_content = data.get('fileContent')
-    print(type(file_content))
-    print(file_content)
-
-    return "upload successful"
+    filecontent = data.get('fileContent')
+    filename = data.get('filename')
+    df = model_predict(filecontent).sort_values('Predicted IC50 value (nM)')
+    upload = Upload(filename=filename,data = filecontent)
+    db.session.add(upload)
+    db.session.commit()
+    df.to_csv('models/acetylcholinesterase/data/user_data_'+str(upload.id)+'.csv',index=False)
+    print('success')
+    return render_template('results.html',headings = list(df),data = df.values.tolist())
 
 if __name__=='__main__':
     app.run(debug=True)
